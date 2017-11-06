8e9009304733749f041d1f8bf7dd5e63ebf7c0cb
Robert Rauch <mail@robertrauch.de>
2017-11-06 13:04:33 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='fermifock',
+      version='0.1',
+      description='library for symbolic computations with ferminonic fock space',
+      author='Robert Rauch',
+      author_email='mail@robertrauch.de',
+      license='MIT',
+      packages=['fermifock'],
+      zip_safe=False)
